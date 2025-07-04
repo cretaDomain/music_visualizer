5447e2fa4e46da36c3d862750c82fe3f6a553ed3
skpark33 <skpark@sqisoft.com>
2025-07-04 23:45:51 +0900

    2-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,14 +29,40 @@ class VisualizerPage extends StatefulWidget {
 }
 
 class _VisualizerPageState extends State<VisualizerPage> {
+  String _message = 'Checking permissions...';
+  // ignore: unused_field
+  bool _hasPermission = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission();
+  }
+
+  Future<void> _requestPermission() async {
+    final status = await Permission.microphone.request();
+    if (status == PermissionStatus.granted) {
+      setState(() {
+        _hasPermission = true;
+        _message = 'Permission granted! Starting audio stream...';
+      });
+      // TODO: Start audio stream here
+    } else {
+      setState(() {
+        _hasPermission = false;
+        _message = 'Permission denied. Please grant microphone access in settings.';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
         child: Text(
-          'Visualizer will be here',
-          style: TextStyle(color: Colors.white),
+          _message,
+          style: const TextStyle(color: Colors.white),
         ),
       ),
     );
